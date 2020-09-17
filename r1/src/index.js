5cc69d06f4052b82cf9c8bd0c06b081ab126faf5
wgr <cwlwwl@163.com>
2020-09-17 14:58:50 +0800

    r1

---
+++ r1/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker.js';
+
+function GetUl() {
+  return(
+	<>
+		<ul>
+			<li>苏格拉底</li>
+			<li>老子</li>
+			<li>修莫</li>
+			<li>亚里士多德</li>
+		</ul>
+		<div>
+			审是
+		</div>
+	</>
+  );
+}
+
+ReactDOM.render(
+	<React.StrictMode>
+		<GetUl />
+	</React.StrictMode>
+	, 
+	document.getElementById('root')  
+);
+
+
+// ReactDOM.render(
+// 	<React.StrictMode>
+// 		<div>
+// 			<button>React.StrictMode是要求代码采用严格模式</button> <br />
+// 			<ul>
+// 				<li>小五</li>
+// 				<li>小王</li>
+// 			</ul>
+// 		</div>
+// 	</React.StrictMode>
+// 	, 
+// 	document.getElementById('root')  
+// );
+
+serviceWorker.unregister();
